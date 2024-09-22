ca1df5100297aa5e1e74ffc597cf4ca6496c1e23
xuangand854 <dangphxse161782@fpt.edu.vn>
2024-09-22 10:46:01 +0700

    Init_Project

---
+++ app/src/main/java/fu/se/spotifi/PlayingMusic.java
@@ -0,0 +1,36 @@
+package fu.se.spotifi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlayingMusic extends AppCompatActivity {
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_playing_music);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent i = new Intent(this, relativeLogin.class);
+        btn = findViewById(R.id.ButtonTest);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayingMusic.this, relativeLogin.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
