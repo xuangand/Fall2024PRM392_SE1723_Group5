f3aa3a2dac5b5ab6bf560af0ac22863557b4669c
Tôn Thất Trung Hiếu <trunghieutonthat@gmail.com>
2024-09-26 20:58:48 +0700

    update
    add sign in layout

---
+++ app/src/main/java/fu/se/spotifi/SignInActivity.java
@@ -0,0 +1,18 @@
+package fu.se.spotifi;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
